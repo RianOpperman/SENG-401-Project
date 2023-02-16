cddf635e186cb2fd7738c48aebb091b9303f8228
ILLEIST <rlopperman@gmail.com>
2023-02-15 18:00:16 -0700

    Testing things

--- App/hello.js
+++ App/hello.js
@@ -1,3 +1,20 @@
-function test() {
-    console.log("From another file");
+class A {
+    a;
+    constructor(x){
+        this.a = x;
+    }
 }
+
+let B = {
+    b: 5,
+    test1: function () {
+        return this.b;
+    },
+    test2: (self) => {
+        return self.b;
+    }
+}
+
+console.log(B.b);
+console.log(B.test1());
+console.log(B.test2(B));
